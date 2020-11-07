22527e7d5471a761a3c68296d4795adb5219a631
AliReza Beigy <AliRezaBeigyKhu@gmail.com>
2020-11-07 10:56:57 +0330

    move App component to app folder

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p
-          className="App-name"
-        >
-          Ticksy
-        </p>
+        <p className="App-name">Ticksy</p>
       </header>
     </div>
   );
