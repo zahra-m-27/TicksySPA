49a258d5da6a8f2dba5c9b2c7503f1a25c253ae8
zahra_m <zahramazhar78@gmail.com>
2020-11-12 22:40:25 +0330

    add router to App component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import Home from "./Pages/Home";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import NotFound from "./Pages/NotFound";
+
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <nav>
+                    <ul style={{listStyle: "none", textAlign: "center"}}>
+                        <li style={{display: "inline-block", paddingRight: 20}}>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li style={{display: "inline-block"}}>
+                            <Link to="/test">Test</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/test">
+                        <NotFound/>
+                    </Route>
+                    <Route path="/">
+                        <Home/>
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
