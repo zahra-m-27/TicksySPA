bf6f3b6e44c463304b9c2f0fd02ae32d713420aa
zahra_m <zahramazhar78@gmail.com>
2020-11-12 22:36:28 +0330

    add react-router-dom package and implement NotFound page

---
+++ src/app/Pages/NotFound/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../NotFound/styles.module.scss";
+
+function NotFound() {
+    return (
+        <div>
+            <p className={styles.NotFound}>Not Found 404</p>
+        </div>
+    );
+}
+
+export default NotFound;
