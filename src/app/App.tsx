bfd778d2256bf4e824e2a238a022cc88c80791ac
TaherehKhodadadi <64657406+TaherehKhodadadi@users.noreply.github.com>
2020-11-15 16:14:37 +0330

    Sign-In

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,34 +1,37 @@
 import React from "react";
 import Home from "./Pages/Home";
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import NotFound from "./Pages/NotFound";
-
+import SignInPage from "./Pages/SignIn";
 
 function App() {
-    return (
-        <div>
-            <Router>
-                <nav>
-                    <ul style={{listStyle: "none", textAlign: "center"}}>
-                        <li style={{display: "inline-block", paddingRight: 20}}>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li style={{display: "inline-block"}}>
-                            <Link to="/test">Test</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Switch>
-                    <Route path="/test">
-                        <NotFound/>
-                    </Route>
-                    <Route path="/">
-                        <Home/>
-                    </Route>
-                </Switch>
-            </Router>
-        </div>
-    );
+  return (
+    <div>
+      <Router>
+        <nav>
+          <ul style={{ listStyle: "none", textAlign: "center" }}>
+            <li style={{ display: "inline-block", paddingRight: 20 }}>
+              <Link to="/">Home</Link>
+            </li>
+            <li style={{ display: "inline-block" }}>
+              <Link to="/test">Test</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/test">
+            <NotFound />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/">
+            <SignInPage />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/app/Pages/SignIn/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+function SignInPage() {
+    return (
+        <div >
+
+
+        </div>
+    );
+}
+
+export default SignInPage;
