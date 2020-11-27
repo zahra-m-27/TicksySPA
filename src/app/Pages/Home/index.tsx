d338984be7978287de217b41384b77e50ea10347
AliReza Beigy <AliRezaBeigyKhu@gmail.com>
2020-11-27 21:35:02 +0330

    Add Link to SignUp page

--- src/app/Pages/Home/index.tsx
+++ src/app/Pages/Home/index.tsx
@@ -10,21 +10,14 @@ export default function HomePage() {
         <img className={styles.landing_image} src={LandingImage} />
         <nav className={styles.nav}>
           <ul>
-            <li>
-              <Link to="/sign-in" className={styles.nav_link}>
-                ورود/ثبت نام
-              </Link>
+            <li className={styles.nav_link}>
+              <Link to="/sign-in">ورود</Link>/<Link to="/sign-up">ثبت نام</Link>
             </li>
-            <li>
-              <Link to="/dashboard" className={styles.nav_link}>
-                داشبورد
-              </Link>
+            <li className={styles.nav_link}>
+              <Link to="/dashboard">داشبورد</Link>
             </li>
-
-            <li>
-              <Link to="/..." className={styles.nav_link}>
-                ارتباط با ما
-              </Link>
+            <li className={styles.nav_link}>
+              <Link to="/contact-us">ارتباط با ما</Link>
             </li>
           </ul>
         </nav>
