11cf40fc9ac58d9a4d3ed82ab7457f65a7bed1b2
zahra_m <zahramazhar78@gmail.com>
2020-11-27 19:33:53 +0330

    add home page

--- src/app/Pages/Home/index.tsx
+++ src/app/Pages/Home/index.tsx
@@ -1,74 +1,30 @@
 import React from "react";
 import styles from "./styles.module.scss";
-import Assets from "../../Assets";
 import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
-    <div className={styles.App}>
-      <header className={styles.AppHeader}>
-        <nav>
-          <ul
-            style={{
-              listStyle: "none",
-              textAlign: "center",
-            }}
-          >
-            <li style={{ display: "inline-block", paddingRight: 20 }}>
-              <Link
-                to="/"
-                style={{
-                  color: "#61dafb",
-                }}
-              >
-                Home
-              </Link>
-            </li>
-            <li style={{ display: "inline-block" }}>
-              <Link
-                to="/404"
-                style={{
-                  color: "#61dafb",
-                }}
-              >
-                NotFound
-              </Link>
-            </li>
-            <li style={{ display: "inline-block", paddingLeft: 20 }}>
-              <Link
-                to="/sign-in"
-                style={{
-                  color: "#61dafb",
-                }}
-              >
-                SignIn
-              </Link>
-            </li>
-            <li style={{ display: "inline-block", paddingLeft: 20 }}>
-              <Link
-                to="/sign-up"
-                style={{
-                  color: "#61dafb",
-                }}
-              >
-                SignUp
-              </Link>
-            </li>
-            <li style={{ display: "inline-block", paddingLeft: 20 }}>
-              <Link
-                to="/forgot-password"
-                style={{
-                  color: "#61dafb",
-                }}
-              >
-                Forgot Password
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <Assets.Svgs.ReactLogoSvg className={styles.AppLogo} />
-        <p className={styles.AppName}>Ticksy</p>
-      </header>
+    <div className={styles.container}>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link to="/sign-in" className={styles.nav_link}>
+              ورود/ثبت نام
+            </Link>
+          </li>
+          <li>
+            <Link to="/dashboard" className={styles.nav_link}>
+              داشبورد
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/..." className={styles.nav_link}>
+              ارتباط با ما
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
