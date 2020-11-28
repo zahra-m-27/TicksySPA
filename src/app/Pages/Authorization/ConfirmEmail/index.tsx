114d5b571d2cbf1daee3fad6f5d0cb404e00137f
AliReza Beigy <AliRezaBeigyKhu@gmail.com>
2020-11-28 12:00:00 +0330

    Fix compile error

--- src/app/Pages/Authorization/ConfirmEmail/index.tsx
+++ src/app/Pages/Authorization/ConfirmEmail/index.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import styles from "./styles.module.scss";
-import ConfirmEmailInputBox from "./Components/ConfirmEmailInputBox";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function ConfirmEmailPage() {
-    return (
-        <div className={styles.container}>
-            <div className={styles.message_box}>
-                <p className={styles.message}>تایید حساب شما با موفقیت انجام شد از سایت خودتون لذت ببرید</p>
-            </div>
-            <Link
-                to="../../../home/styles.module.scss">
-                صفحه اصلی
-            </Link>
-            <ConfirmEmailInputBox className={styles.input_box_container} />
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <div className={styles.message_box}>
+        <p className={styles.message}>
+          تایید حساب شما با موفقیت انجام شد از سایت خودتون لذت ببرید
+        </p>
+      </div>
+      <Link to="">صفحه اصلی</Link>
+    </div>
+  );
 }
--- src/app/Pages/Authorization/index.tsx
+++ src/app/Pages/Authorization/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import SignInPage from "./SignIn";
 import SignUpPage from "./SignUp";
+import styles from "./styles.module.scss";
+import ConfirmEmailPage from "./ConfirmEmail";
 import ForgotPasswordPage from "./ForgotPassword";
 import ChangePasswordPage from "./ChangePassword";
-import ConfirmEmailpage from "./Components/ConfirmEmailInputBox";
-import styles from "./styles.module.scss";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function Authorization() {
@@ -16,9 +16,9 @@ export default function Authorization() {
         <Switch>
           <Route path="/sign-in" component={SignInPage} />
           <Route path="/sign-up" component={SignUpPage} />
+          <Route path="/confirm-email" component={ConfirmEmailPage} />
           <Route path="/forgot-password" component={ForgotPasswordPage} />
           <Route path="/change-password" component={ChangePasswordPage} />
-          <Route path="/confirm-email" component={ConfirmEmailPage} />
         </Switch>
       </Router>
     </div>
