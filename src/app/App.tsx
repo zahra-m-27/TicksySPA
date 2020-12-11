2be8128f7d15801d6bbf2a863747c23d09c0dd52
TaherehKhodadadi <64657406+TaherehKhodadadi@users.noreply.github.com>
2020-12-12 01:06:46 +0330

    create-ticket

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "./Pages/Home";
 import NotFound from "./Pages/NotFound";
 import Authorization from "./Pages/Authorization";
+import CreateTicket from "./Pages/CreateTicket";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
           component={Authorization}
         />
         <Route path="/404" component={NotFound} />
+        <Route path="/ticket/new" component={CreateTicket} />
         <Route path="/" component={Home} />
       </Switch>
     </Router>
