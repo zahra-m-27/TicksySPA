34391b6cedcefab0b79045c1f720292f4706fcd3
AliRezaBeigy <AliRezaBeigyKhu@gmail.com>
2020-12-12 22:42:33 +0330

    Update .gitignore Fix conflict

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import Home from "./Pages/Home";
 import NotFound from "./Pages/NotFound";
-import Authorization from "./Pages/Authorization";
-<<<<<<< HEAD
-import CreateTicket from "./Pages/CreateTicket";
-=======
 import Dashboard from "./Pages/Dashboard";
->>>>>>> 87cadf08f4995d5b1c7987228063928aab276cdf
+import CreateTicket from "./Pages/CreateTicket";
+import Authorization from "./Pages/Authorization";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
