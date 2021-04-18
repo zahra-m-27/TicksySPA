493558a84ee6e3b9bcba6f93826980c9dab177da
AliRezaBeigy <AliRezaBeigyKhu@gmail.com>
2021-04-18 17:16:37 +0430

    Implement TextArea

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,16 @@ import ReactDOM from 'react-dom';
 import {ConfigProvider} from 'antd';
 import reportWebVitals from './reportWebVitals';
 
+import 'froala-editor/js/languages/fa.js';
+import 'font-awesome/css/font-awesome.css';
+import 'froala-editor/js/plugins.pkgd.min.js';
+import 'froala-editor/css/froala_style.min.css';
+import 'froala-editor/js/third_party/embedly.min.js';
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+import 'froala-editor/js/third_party/image_tui.min.js';
+import 'froala-editor/js/third_party/spell_checker.min.js';
+import 'froala-editor/js/third_party/font_awesome.min.js';
+
 ReactDOM.render(
   <React.StrictMode>
     <ConfigProvider direction="rtl">
