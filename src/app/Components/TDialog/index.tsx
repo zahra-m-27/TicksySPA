2192a7b6dd0eec77b7ecfcc686d3d4f982165f3a
mostafa <mostafakhoshnood99@gmail.com>
2021-06-09 14:05:05 +0430

    Update TDialog Component

--- src/app/Components/TDialog/index.tsx
+++ src/app/Components/TDialog/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {CSSProperties} from 'react';
 import ReactDOM from 'react-dom';
 import Assets from '../../Assets';
 import styles from './styles.module.scss';
@@ -6,11 +6,17 @@ import ClassNames from '../../Utilities/ClassNames';
 
 interface ITDialog {
   className?: string;
+  style?: CSSProperties;
   onDismiss?: () => void;
   content: React.ReactNode;
 }
 
-export default function showDialog({className, content, onDismiss}: ITDialog) {
+export default function showDialog({
+  style,
+  content,
+  className,
+  onDismiss,
+}: ITDialog) {
   let container = document.getElementById('TDialog');
 
   if (!container) {
@@ -31,7 +37,13 @@ export default function showDialog({className, content, onDismiss}: ITDialog) {
   };
 
   ReactDOM.render(
-    <TDialog className={className} onDismiss={onClose} content={content} />,
+    <TDialog
+      style={style}
+      content={content}
+      onDismiss={onClose}
+      className={className}
+      showDismiss={!!onDismiss}
+    />,
     element
   );
   container.appendChild(element);
@@ -39,18 +51,27 @@ export default function showDialog({className, content, onDismiss}: ITDialog) {
   return onClose;
 }
 
-function TDialog({className, content, onDismiss}: ITDialog) {
+function TDialog({
+  style,
+  content,
+  className,
+  onDismiss,
+  showDismiss,
+}: ITDialog & {showDismiss: boolean}) {
   return (
     <div className={styles.t_dialog} onClick={() => onDismiss && onDismiss()}>
       <div className={styles.t_dialog_bg} />
       <div
+        style={style}
         className={ClassNames(styles.t_dialog_container, className)}
         onClick={(e) => e.stopPropagation()}>
-        <span
-          className={styles.t_dialog_multiply_box}
-          onClick={() => onDismiss && onDismiss()}>
-          <img src={Assets.SVGs.Close} className={styles.t_dialog_multiply} />
-        </span>
+        {showDismiss && (
+          <span
+            className={styles.t_dialog_multiply_box}
+            onClick={() => onDismiss && onDismiss()}>
+            <img src={Assets.SVGs.Close} className={styles.t_dialog_multiply} />
+          </span>
+        )}
         {content}
       </div>
     </div>
