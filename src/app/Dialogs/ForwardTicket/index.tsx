680ffd01c919dabc50840d1521c7cbfc4e3e091e
AliRezaBeigy <AliRezaBeigyKhu@gmail.com>
2021-06-10 18:47:26 +0430

    Add ForwardTicker Dialog

---
+++ src/app/Dialogs/ForwardTicket/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss';
+
+export default function ForwardDialog() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}></div>
+      <div className={styles.footer}></div>
+    </div>
+  );
+}
