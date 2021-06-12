f38ec9d208606a3f5b4867a3ca8a73b4e84eb0bd
erfan zare <erfun.zare7@gmail.com>
2021-06-13 01:02:42 +0430

    Add TDropDown Test

--- src/app/Components/TDropDown/index.test.tsx
+++ src/app/Components/TDropDown/index.test.tsx
@@ -1,9 +1,16 @@
 import TDropDown from './index';
 import React from 'react';
 import {render} from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import user from '@testing-library/user-event';
+
+const onSelect = jest.fn();
 
 const sample = (
   <TDropDown
+    onSelect={onSelect}
+    label="Test Label"
     items={[
       {
         value: 'val1',
@@ -20,9 +27,33 @@ const sample = (
     ]}
   />
 );
-test('TDrop', () => {
+
+test('render in ReactDOM', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(sample, div);
+});
+
+test('render items', () => {
   const {getByText} = render(sample);
   getByText(/تست1/i);
   getByText(/تست2/i);
   getByText(/تست3/i);
+});
+
+test('render label', () => {
+  const {getByText} = render(sample);
+  getByText(/Test Label/i);
+});
+
+test('onSelect drop down', () => {
+  const {getByText} = render(sample);
+  const item1 = getByText(/تست1/i);
+  user.click(item1);
+  expect(onSelect).toBeCalledTimes(1);
+  expect(onSelect).toBeCalledWith('val1');
+});
+
+it('snapshot', () => {
+  const snapshot = renderer.create(sample).toJSON();
+  expect(snapshot).toMatchSnapshot();
 });
