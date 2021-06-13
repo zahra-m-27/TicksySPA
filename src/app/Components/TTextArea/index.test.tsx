19645ff6d7ef235a9a3f7c9295eb8d6a8948ef87
zahra_m <zahramazhar78@gmail.com>
2021-06-13 19:11:52 +0430

    Update TTextArea Test

--- src/app/Components/TTextArea/index.test.tsx
+++ src/app/Components/TTextArea/index.test.tsx
@@ -1,20 +1,36 @@
 import React from 'react';
-import {render, fireEvent} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import {render} from '@testing-library/react';
 import TTextArea from './index';
+import ReactDOM from 'react-dom';
+import user from '@testing-library/user-event';
+import renderer from 'react-test-renderer';
+
+const onChange = jest.fn();
 
 const sample = (
   <TTextArea
-    content="Text area"
-    placeholder="Text area"
-    onChange={() => 'some text'}
+    content="Test Content"
+    placeholder="Test placeholder"
+    onChange={onChange}
   />
 );
-test('TTextArea', () => {
-  const {getByText, getByTestId, getByLabelText} = render(sample);
-  const input = getByLabelText('some text');
-  input.value = 'Text area';
-  fireEvent.change(input);
-  fireEvent.click(getByText('Text area'));
-  expect(getByTestId('Text area')).toHaveTextContent('Text area');
+
+test('render in ReactDOM', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(sample, div);
 });
+
+// test('render content', () => {
+//   const {getByText} = render(sample);
+//   getByText('Test Content');
+// });
+
+// test('change text', () => {
+//   const {getByText} = render(sample);
+//   const inputElement = getByText('Test Content') as HTMLTextAreaElement;
+//
+//   user.clear(inputElement);
+//   expect(inputElement.value).toEqual('');
+//   user.type(inputElement, 'Test Typing');
+//   expect(inputElement.value).toEqual('Test Typing');
+// });
