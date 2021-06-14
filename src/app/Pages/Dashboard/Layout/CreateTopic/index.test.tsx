b70690d98d1faf1fa5cc8f351c6a7afa7e91fef6
zahra_m <zahramazhar78@gmail.com>
2021-06-14 18:22:54 +0430

    Add Create Topic Page Test

---
+++ src/app/Pages/Dashboard/Layout/CreateTopic/index.test.tsx
@@ -0,0 +1,47 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import {render} from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import {MemoryRouter} from 'react-router-dom';
+import CreateTopic from './index';
+
+const sample = (
+  <MemoryRouter>
+    <CreateTopic />
+  </MemoryRouter>
+);
+
+test('render in ReactDOM', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(sample, div);
+});
+
+it('render header', () => {
+  const {getByText} = render(sample);
+  getByText(/تاپيک جدید/i);
+});
+
+it('render subject', () => {
+  const {getByText} = render(sample);
+  getByText(/عنوان/i);
+});
+
+it('render username', () => {
+  const {getByText} = render(sample);
+  getByText(/شناسه/i);
+});
+
+it('render description', () => {
+  const {getByText} = render(sample);
+  getByText(/توضيح/i);
+});
+
+it('render submit', () => {
+  const {getByText} = render(sample);
+  getByText(/ثبت/i);
+});
+
+it('snapshot', () => {
+  const snapshot = renderer.create(sample).toJSON();
+  expect(snapshot).toMatchSnapshot();
+});
