51bab71845c0c11b5af6101a621e412a5386f0a4
erfan zare <erfun.zare7@gmail.com>
2021-06-18 22:12:12 +0430

    Update Tickets API

--- src/app/API/Domains/Tickets.ts
+++ src/app/API/Domains/Tickets.ts
@@ -1,24 +1,22 @@
-import {Post} from '../fetch';
+import {Get, Post} from '../fetch';
 import GetTicketsViewModel from '../ViewModels/Tickets/GetTicketsViewModel';
 import GetTicketDetailViewModel from '../ViewModels/Tickets/GetTicketViewModel';
+import CreateTicketViewModel from '../ViewModels/Tickets/CreateTicketViewModel';
 import CreateTicketMessageViewModel from '../ViewModels/Tickets/CreateTicketMessageViewModel';
+import UpdateMessageRateViewModel from '../ViewModels/Tickets/UpdateMessageRateViewModel';
+import EditTicketViewModel from '../ViewModels/Tickets/EditTicketViewModel';
 
-const ControllerName = 'tickets';
+const ControllerName = 'ticket';
 
 function GetTicket(args: GetTicketDetailViewModel.Request) {
-  return Post<GetTicketDetailViewModel.Response>(
+  return Get<GetTicketDetailViewModel.Response>(
     ControllerName + `/${args.id}/`,
-    undefined,
-    'GET'
+    {}
   );
 }
 
 function GetTickets(args: GetTicketsViewModel.Request) {
-  return Post<GetTicketsViewModel.Response>(
-    ControllerName + '/?' + new URLSearchParams(args as any).toString(),
-    undefined,
-    'GET'
-  );
+  return Get<GetTicketsViewModel.Response>(ControllerName + '/', args);
 }
 
 function CreateTicketMessage(args: CreateTicketMessageViewModel.Request) {
@@ -27,14 +25,47 @@ function CreateTicketMessage(args: CreateTicketMessageViewModel.Request) {
   for (const attachment of args.attachments)
     formData.append('attachments', attachment);
   return Post<CreateTicketMessageViewModel.Response>(
-    ControllerName + `/${args.id}/`,
+    ControllerName + `/${args.id}/message/`,
     formData
+  );
+}
+
+function CreateTicket(args: CreateTicketViewModel.Request) {
+  const formData = new FormData();
+  formData.append('tags', args.tags);
+  formData.append('text', args.text);
+  formData.append('title', args.title);
+  formData.append('section', args.section.toString());
+  for (const attachment of args.attachments)
+    formData.append('attachments', attachment);
+  formData.append('priority', `${args.priority}`);
+  return Post<CreateTicketViewModel.Response>(
+    ControllerName + `/`,
+    formData,
+    'POST'
+  );
+}
+
+function UpdateMessageRate(args: UpdateMessageRateViewModel.Request) {
+  return Post<UpdateMessageRateViewModel.Response>(
+    `/messages/${args.messageId}/`,
+    args
+  );
+}
+
+function EditTicket(args: EditTicketViewModel.Request) {
+  return Post<EditTicketViewModel.Response>(
+    ControllerName + `/${args.ticketId}/`,
+    args
   );
 }
 
 const Actions = {
   GetTicket,
   GetTickets,
+  EditTicket,
+  CreateTicket,
+  UpdateMessageRate,
   CreateTicketMessage,
 };
 
