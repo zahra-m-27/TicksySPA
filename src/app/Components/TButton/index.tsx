f31f473b0037d15b73890833e49237e77d3db74e
mostafa <mostafakhoshnood99@gmail.com>
2021-06-19 22:42:08 +0430

    Improve TButton Coverage

--- src/app/Components/TButton/index.tsx
+++ src/app/Components/TButton/index.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import styles from './styles.module.scss';
+import ClassNames from '../../Utilities/ClassNames';
 
 interface Props {
   label?: string;
+  className?: string;
   onClick?: () => void;
+  labelClassName?: string;
   backgroundColor?: string;
 }
 
-export default function TButton({onClick, label, backgroundColor}: Props) {
+export default function TButton({
+  label,
+  onClick,
+  className,
+  labelClassName,
+  backgroundColor,
+}: Props) {
   return (
-    <div className={styles.button} onClick={onClick} style={{backgroundColor}}>
-      {label}
+    <div
+      className={ClassNames(styles.button, className)}
+      onClick={onClick}
+      style={{backgroundColor}}>
+      <span className={ClassNames(styles.label, labelClassName)}>{label}</span>
     </div>
   );
 }
