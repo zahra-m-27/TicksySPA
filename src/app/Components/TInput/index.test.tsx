db835524074c714fb88c6e5c7f5c45f14f6a7381
zahra_m <zahramazhar78@gmail.com>
2021-06-20 18:01:12 +0430

    Update TInput Tests

--- src/app/Components/TInput/index.test.tsx
+++ src/app/Components/TInput/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import {render, waitFor} from '@testing-library/react';
 import TInput from './index';
 import ReactDOM from 'react-dom';
 import user from '@testing-library/user-event';
@@ -14,6 +14,28 @@ const sample = (
     content="Test Content"
     onChangeText={onChange}
   />
+);
+const sample2 = (
+  <TInput
+    isNumeric
+    name="number"
+    label="Test Label"
+    content="123456"
+    onChangeText={onChange}
+  />
+);
+const sample3 = (
+  <TInput
+    isNumeric
+    name="number"
+    regex={/\d\d/}
+    content="123456"
+    label="Test Label"
+    onChangeText={onChange}
+  />
+);
+const sample4 = (
+  <TInput name="tags" label="Tags" tags={['tag1']} onChangeText={onChange} />
 );
 
 test('render in ReactDOM', () => {
@@ -49,6 +71,46 @@ it('input onChange', () => {
   expect(onChange).toBeCalledWith('', expect.anything());
   user.type(inputElement, 'Test Typing');
   expect(onChange).toBeCalledWith('Test Typing', expect.anything());
+});
+
+it('number input onChange', async () => {
+  const {getByDisplayValue, getByTestId} = render(sample2);
+  const inputElement = getByDisplayValue(/123456/i);
+  user.clear(inputElement);
+  expect(onChange).toBeCalledWith('', expect.anything());
+  user.type(inputElement, '1234');
+  expect(onChange).toBeCalledWith('1234', expect.anything());
+  user.clear(inputElement);
+  expect(onChange).toBeCalledWith('', expect.anything());
+  user.type(inputElement, 'R');
+  await waitFor(() =>
+    expect(getByTestId('input-container').className).toContain(
+      'failed_regex_focused_input_container'
+    )
+  );
+  user.clear(inputElement);
+  user.type(inputElement, '124');
+  expect(onChange).toBeCalledWith('124', expect.anything());
+});
+
+it('regex check', () => {
+  const {getByDisplayValue} = render(sample3);
+  const inputElement = getByDisplayValue(/123456/i);
+  user.clear(inputElement);
+  expect(onChange).toBeCalledWith('', expect.anything());
+  user.type(inputElement, '1234');
+});
+
+test('renders tags', () => {
+  const {getByText} = render(sample4);
+
+  const tag = getByText(/tag1/i);
+
+  const closeTag = tag.querySelector('.anticon-close');
+
+  if (closeTag) {
+    user.click(closeTag);
+  }
 });
 
 it('snapshot Input', () => {
